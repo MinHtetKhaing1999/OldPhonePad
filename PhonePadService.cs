2f27c0ecb59ddacb70f4823a6b40a7bfceefe69f
MinHtetKhaing1999 <131604216+MinHtetKhaing1999@users.noreply.github.com>
2025-02-18 03:34:21 +0700

    Add files via upload

---
+++ PhonePadService.cs
@@ -0,0 +1,61 @@
+﻿using System;
+namespace PhoneKeyPad
+{
+	public class PhonePadService
+	{
+		private static readonly Dictionary<char, char[]> values = new Dictionary<char, char[]>();
+        
+        public PhonePadService()
+        {
+            values.TryAdd('1', new char[] { '&', '\'', '(' });
+            values.TryAdd('2', new char[] { 'A', 'B', 'C' });
+            values.TryAdd('3', new char[] { 'D', 'E', 'F' });
+            values.TryAdd('4', new char[] { 'G', 'H', 'I' });
+            values.TryAdd('5', new char[] { 'J', 'K', 'L' });
+            values.TryAdd('6', new char[] { 'M', 'N', 'O' });
+            values.TryAdd('7', new char[] { 'P', 'Q', 'R', 'S' });
+            values.TryAdd('8', new char[] { 'T', 'U', 'V' });
+            values.TryAdd('9', new char[] { 'W', 'X', 'Y', 'Z' });
+            values.TryAdd('0', new char[] { ' ' });
+        }
+
+		public String OldPhonePad(string input)
+		{
+            string data = string.Empty;
+            int pressCount = 1;
+            char prevChar = char.MinValue;
+            foreach(char c in input)
+            {
+                if(c == prevChar || c == '*')
+                {
+                    data = data.Remove(data.Length - 1);
+                    ++pressCount;
+                }
+                else
+                {
+                    pressCount = 1;
+                }
+                if (c == ' ')
+                {
+                    prevChar = c;
+                    continue;
+                }
+
+                if (c == '#')
+                {
+                    break;
+                }
+                
+                data += GetCharacter(c, pressCount);
+                prevChar = c;
+            }
+            return data;
+		}
+
+        private static char GetCharacter(char input, int pressCount)
+		{
+            return values.Where(x => x.Key == input).Select(x => x.Value[pressCount - 1]).FirstOrDefault();
+		}
+    }
+}
+
